66871cb3864a072de92deed33fb8bcf29aea228e
zerogvt <gkoumplias@gmail.com>
2020-09-18 13:42:55 +0300

    v1

--- hello/hello/settings.py
+++ hello/hello/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_uotcz2=)w(#5al$c4g9+imsmi&y05!s-(v!ez)6ia!km$6u!%'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
